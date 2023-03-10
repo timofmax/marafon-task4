d0458e9137ce017d3ad0a67df4a4f6731ad0c577
timofmax <timofmax@gmail.com>
2023-03-10 15:39:25 +0300

    init

---
+++ marafon-task4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marafon-task4
+//
+//  Created by Timofey Privalov on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
